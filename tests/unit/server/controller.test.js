e78762364c70a6f84a2272161ca0fc2967ca14c5
Rafael Barros <rafaelnogueirabarr@gmail.com>
2022-03-18 21:00:09 -0300

    challenge > routes > controller with 100% test coverage

--- tests/unit/server/controller.test.js
+++ tests/unit/server/controller.test.js
@@ -3,7 +3,9 @@ import { Controller } from "../../../server/controller";
 import { Service } from "../../../server/service";
 import config from "../../../server/config.js";
 import TestUtil from "../utils/test.util.js";
+import { PassThrough } from "stream";
 import { extname } from "path";
+
 const {
   pages: { homeHTML },
 } = config;
@@ -31,5 +33,63 @@ describe("#Controller - test suite for controller", () => {
     expect(getFileStream).toHaveBeenCalledWith(homeHTML);
     expect(stream).toStrictEqual(mockFileStream);
     expect(type).toStrictEqual(filetype);
+  });
+  test("handleCommand - given the start command it should call service method", async () => {
+    const controller = new Controller();
+    const command = {
+      command: "start",
+    };
+    const expectedResult = {
+      result: "ok",
+    };
+    const startStreaming = jest.spyOn(
+      Service.prototype,
+      Service.prototype.startStreaming.name
+    );
+
+    const result = await controller.handleCommand(command);
+    expect(result).toStrictEqual(expectedResult);
+    expect(startStreaming).toHaveBeenCalled();
+  });
+  test("handleCommand - given the stop command it should call service method", async () => {
+    const controller = new Controller();
+    const command = {
+      command: "stop",
+    };
+    const expectedResult = {
+      result: "ok",
+    };
+    const startStreaming = jest.spyOn(
+      Service.prototype,
+      Service.prototype.stopStreaming.name
+    );
+
+    const result = await controller.handleCommand(command);
+    expect(result).toStrictEqual(expectedResult);
+    expect(startStreaming).toHaveBeenCalled();
+  });
+
+  test("createClientStream - it should returns an stream and onCloseFunction", async () => {
+    const controller = new Controller();
+    const mockClientStream = new PassThrough();
+    const mockID = "dasdsa";
+    const onClose = jest.fn();
+
+    const expectedResult = {
+      onClose,
+      stream: mockClientStream,
+    };
+
+    jest
+      .spyOn(Service.prototype, Service.prototype.createClientStream.name)
+      .mockReturnValue({ clientStream: mockClientStream, id: mockID });
+    jest.spyOn(Service.prototype, Service.prototype.removeClientStream.name);
+
+    const result = controller.createClientStream();
+    result.onClose();
+
+    expect(Service.prototype.createClientStream).toHaveBeenCalled();
+    expect(result.stream).toStrictEqual(expectedResult.stream);
+    expect(Service.prototype.removeClientStream).toHaveBeenCalledWith(mockID);
   });
 });
